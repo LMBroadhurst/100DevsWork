b702117e5d2cdc740a168effebf85369355613fa
Lewis Broadhurst <lewis1broadhurst@gmail.com>
2022-06-09 17:46:17 +0100

    NASA API

--- class27-materials/api-template/js/main.js
+++ class27-materials/api-template/js/main.js
@@ -2,13 +2,16 @@
 document.querySelector('button').addEventListener('click', getFetch)
 
 function getFetch(){
-  const choice = document.querySelector('input').value
-  const url = 'https://pokeapi.co/api/v2/pokemon/'+choice
+  const choice = document.querySelector('input').value;
+  console.log(choice);
+  const url = `https://api.nasa.gov/planetary/apod?api_key=xxnvk8iJdTqGODTWPx0s3CxpzMeIvnwtABdVyOX3&date=${choice}`;
 
   fetch(url)
       .then(res => res.json()) // parse response as JSON
       .then(data => {
         console.log(data)
+        document.querySelector('img').src = data.hdurl;
+        document.querySelector('h3').innerText = data.explanation;
       })
       .catch(err => {
           console.log(`error ${err}`)
