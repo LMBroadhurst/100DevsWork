9ae947a285a15e773f452199ec9e1d46ee8a03f4
Lewis Broadhurst <lewis1broadhurst@gmail.com>
2022-06-09 16:51:45 +0100

    finished class 26

--- class26-materials/objects-shots/js/main.js
+++ class26-materials/objects-shots/js/main.js
@@ -1,14 +1,35 @@
 //The user will enter a cocktail. Get a cocktail name, photo, and instructions and place them in the DOM
 
-const url = "www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita";
+document.querySelector('button').addEventListener('click', getDrink);
+let drinkVal = 0;
 
-fetch("https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita")
+function getDrink() {
+
+    let drink = document.querySelector('input').value;
+    
+
+    fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${drink}`)
     .then(response => response.json())
     .then(data => {
         console.log(data.drinks);
-        document.querySelector("h2").innerText = data.drinks[0].strDrink;
-        document.querySelector("h3").innerText = data.drinks[0].strInstructions;
+        document.querySelector("h2").innerText = data.drinks[drinkVal].strDrink;
+        document.querySelector("h3").innerText = data.drinks[drinkVal].strInstructions;
+        document.querySelector('img').src = data.drinks[drinkVal].strDrinkThumb;
+
+        if (drinkVal < data.drinks.length - 1) {
+            drinkVal++;
+        } else {
+            drinkVal = 0;
+        }
+        
     })
     .catch(err => {
         console.log(`Error = ${err}`)
     })
+
+}
+
+
+
+
+
