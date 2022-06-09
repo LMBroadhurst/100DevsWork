a70bccf0b73d557bd27b00557f601da83bf282dc
Lewis Broadhurst <lewis1broadhurst@gmail.com>
2022-06-09 16:21:29 +0100

    created dog api request

--- class26-materials/objects-dog-photos/js/main.js
+++ class26-materials/objects-dog-photos/js/main.js
@@ -1,1 +1,14 @@
 //Get a dog photo from the dog.ceo api and place the photo in the DOM
+
+
+fetch('https://dog.ceo/api/breeds/image/random')
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+        console.log(data.message);
+        console.log(data.status)
+        document.querySelector('img').src = data.message;
+    })
+    .catch(err => {
+        console.log(`Error = ${err}`)
+    });
